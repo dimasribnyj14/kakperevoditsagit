4801f787472ee0573ccc11b5c7c8fb5bead1be9b
Dima Sribnyj <77404615+dimasribnyj14@users.noreply.github.com>
2022-10-20 19:48:56 +0300

    おい

---
+++ main.py
@@ -0,0 +1,10 @@
+original = 'git'
+language_original = "английском"
+translate = 'мерзавец'
+language_translate = 'русском'
+if original == 'git':
+    print('Оригинал на', language_original, 'языке:')
+    print(original)
+    print("Перевод на", language_translate, 'языке:')
+    print(translate)
+print("Мы перевели 'git'.")
